6fe5a61ca7b69dc2567755c11c46a2b7b6bb373c
Zero-i00 <clean.shiro@gmail.com>
2023-03-20 14:24:28 +0500

    Initial commit

---
+++ app/src/main/java/com/example/practice13/OnboardingScreen.kt
@@ -0,0 +1,11 @@
+package com.example.practice13
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class OnboardingScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_onboarding_screen)
+    }
+}
---
+++ app/src/main/java/com/example/practice13/SplashScreen.kt
@@ -0,0 +1,11 @@
+package com.example.practice13
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
